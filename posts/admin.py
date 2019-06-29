6da855f5eb09c3e06e25bf66080d8fbdc6db79b1
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-06-29 09:42:07 +0100

    Posts and comments are displayed jointly in admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+    def get_extra(self, request, obj=None, **kwargs):
+        return 1
+
+    fieldsets = (
+        (None, {
+            "fields": ("user", "content",)
+        }),
+    )
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+    fieldsets = (
+        (None, {
+            "fields": ("user", "content",)
+        }),
+    )
+
+    list_display = ("user", "created_at", "likes_count", "comments_count",)
+    list_filter = ("likes_count", "comments_count",)
+    
+
+
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
